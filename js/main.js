5851893b6ae8fe0371ca566b9345ff3246677e2d
oluwajhay <lordsylharx@gmail.com>
2019-01-30 10:41:31 +0100

    fixed shadow

--- js/main.js
+++ js/main.js
@@ -120,3 +120,23 @@
         })
     })
 }(jQuery);
+
+
+$(document).ready(function() {
+    $(this).on("click", ".cd-faq-trigger", function() {
+      $(this).parent().find(".cd-faq-content").toggle();
+      $(this).find(".fa").toggleClass('active');
+    });
+  });
+
+  $(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".navbar").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".navbar").removeClass("active");
+        }
+    });
+});
+
